c9832ff81e19cb27ca3c11c996bfb4e66d53c63b
mido4315 <mohmed.mahmoud3@gmail.com>
2023-04-22 16:46:22 +0200

    create books model

---
+++ eBookStore/eBookStore/Models/Books.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eBookStore.Models
+{
+	public class Books
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string Title { get; set; }
+		[Required]
+		public string Description { get; set; }
+		public string Author { get; set; }
+		[Required]
+		public double Price { get; set; }
+		public float Rating { get; set; }
+		public string Category { get; set; }
+	}
+}
