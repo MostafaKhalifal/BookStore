c252ac2e1f50f5275cd7b3d45c401257bf436aec
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-12 18:33:31 +0300

    test

--- eBookStore/eBookStore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace eBookStore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
